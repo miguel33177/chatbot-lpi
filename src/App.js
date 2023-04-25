21cde0032c4e463458001247026f122bacd93067
Bruno <41094@ufp.edu.pt>
2023-04-25 23:21:33 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FloatingButton from "./FloatingButton";
 import config from "./chatbot/config";
 import ActionProvider from "./chatbot/ActionProvider";
 import MessageParser from "./chatbot/MessageParser";
-import "./App.css";
+
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
