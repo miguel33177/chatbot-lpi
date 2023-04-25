2e0b8b4f4c081186627a95427e5335ded67eda46
Bruno <41094@ufp.edu.pt>
2023-04-25 23:11:14 +0100

    floatingButton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
-
+import FloatingButton from "./FloatingButton";
 import config from "./chatbot/config";
 import ActionProvider from "./chatbot/ActionProvider";
 import MessageParser from "./chatbot/MessageParser";
-
 import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleChatbot = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="App">
-      <div style={{ maxWidth: "300px" }}>
-        <Chatbot
-          config={config}
-          actionProvider={ActionProvider}
-          messageParser={MessageParser}
-        />
-      </div>
+      <FloatingButton isOpen={isOpen} handleClick={handleToggleChatbot} />
+      {isOpen && (
+        <div className="chatbot-container">
+          <Chatbot
+            config={config}
+            actionProvider={ActionProvider}
+            messageParser={MessageParser}
+          />
+        </div>
+      )}
     </div>
   );
 }
